92887b9a9b65fd2adc139636b34aca132f7fa03e
nivedhanr <nivedharenahan@gmail.com>
2022-11-25 17:10:57 +0530

    addcourse

---
+++ src/app/addcousre/addcousre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addcousre',
+  templateUrl: './addcousre.component.html',
+  styleUrls: ['./addcousre.component.css']
+})
+export class AddcousreComponent {
+
+}
